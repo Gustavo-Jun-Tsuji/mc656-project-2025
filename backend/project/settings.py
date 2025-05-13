4c32a26c2e95b129c25672d81743435b567f38e9
romcol <e.rcolombo2@gmail.com>
2025-05-13 15:22:15 -0300

    setup: communication between front and back ends

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(', ')
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
