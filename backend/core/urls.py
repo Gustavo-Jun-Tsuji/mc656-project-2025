dc4ee75d1fe66bee4b3640419fcdf312d42c0c68
Jun <gustavogujun@gmail.com>
2025-05-22 14:08:41 -0300

    refactor(dispensable): delete dead code

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,18 +6,5 @@ router = DefaultRouter()
 router.register(r'routes', views.RouteViewSet)
 
 urlpatterns = [
-    path('test/', views.test),
-    # path("routes/", views.RouteViewSet.as_view({
-    #     "get": "list",
-    #     "post": "create"
-    # }), name='route-list'),
-    # path('routes/<int:pk>/', views.RouteViewSet.as_view({
-    #     'get': 'retrieve',       # GET request to get a specific route
-    #     'delete': 'destroy'      # DELETE request to delete a route
-    # }), name='route-detail'),
-    # # Custom actions
-    # path('routes/search/', views.RouteViewSet.as_view({
-    #     'get': 'search',
-    # }), name='route-search'),
     path('', include(router.urls))
 ]
