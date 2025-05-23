169da0316b6cfba40091debc5bd804a4ad885d49
romcol <e.rcolombo2@gmail.com>
2025-05-23 16:10:29 -0300

    create user serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,20 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Route
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'email': {'required': True},
+            'username': {'required': True}
+        }
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class RouteSerializer(serializers.ModelSerializer):
     distance = serializers.SerializerMethodField()
