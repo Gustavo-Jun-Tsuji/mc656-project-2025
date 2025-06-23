19a535c491eabcf65831e03c42c96bda94422887
Gustavo-Jun-Tsuji <gustavogujun@gmail.com>
2025-06-23 15:14:28 -0300

    fix: images not showing

--- backend/core/management/commands/seed_data.py
+++ backend/core/management/commands/seed_data.py
@@ -89,27 +89,6 @@ class Command(BaseCommand):
                 [-22.814439346281365, -47.05716890930935],
             ],
             tags=["caminhada", "cultural", "lazer", "subida"],
-            image="route_images/obser.jpg",
-        )
-
-        add_route(
-            user=User.objects.get(username="Sarah Pereira"),
-            title="Corrida: Círculo Interno",
-            description="Circuito clássico de 1km ao redor do Ciclo Básico (DCE). Quase todo plano e em asfalto, é perfeito para treinos de velocidade ou para iniciantes. Rota movimentada e arborizada.",
-            starting_location="PB",
-            ending_location="PB ",
-            coordinates=[
-                [-22.817623718567944, -47.071215977613015],
-                [-22.818335741937112, -47.07057266032092],
-                [-22.818474190493244, -47.06935035746595],
-                [-22.81774238938799, -47.06840682543757],
-                [-22.816634791046013, -47.06832104979861],
-                [-22.815823865119555, -47.068942923180956],
-                [-22.81572497138358, -47.07031533340409],
-                [-22.81641722602749, -47.0711516458838],
-                [-22.816911691476015, -47.07128030934222],
-            ],
-            tags=["corrida"],
             image="route_images/unicamp.jpg",
         )
 
@@ -131,7 +110,52 @@ class Command(BaseCommand):
                 [-22.8259897580161, -47.06357904082546],
             ],
             tags=["sombra", "tranquila", "passeio", "longa"],
-            image="route_images/Hospital_de_Clínicas_da_Unicamp_(1).jpg",
+            image="route_images/Praça_CB_Unicamp.jpg",
+        )
+
+        add_route(
+            user=User.objects.get(username="Sara Pereira"),
+            title="Corrida: Círculo Interno",
+            description="Circuito clássico de 1km ao redor do Ciclo Básico (DCE). Quase todo plano e em asfalto, é perfeito para treinos de velocidade ou para iniciantes. Rota movimentada e arborizada.",
+            starting_location="PB",
+            ending_location="PB ",
+            coordinates=[
+                [-22.817623718567944, -47.071215977613015],
+                [-22.818335741937112, -47.07057266032092],
+                [-22.818474190493244, -47.06935035746595],
+                [-22.81774238938799, -47.06840682543757],
+                [-22.816634791046013, -47.06832104979861],
+                [-22.815823865119555, -47.068942923180956],
+                [-22.81572497138358, -47.07031533340409],
+                [-22.81641722602749, -47.0711516458838],
+                [-22.816911691476015, -47.07128030934222],
+            ],
+            tags=["corrida"],
+            image="route_images/unicamp.jpg",
+        )
+
+        add_route(
+            user=User.objects.get(username="Clara Carvalha"),
+            title="Tour dos Bandejões: RU RA RS",
+            description="Uma rota utilitária que interliga os três principais restaurantes universitários (RU, RA e RS). Ideal para calouros ou para quem precisa decidir onde almoçar.",
+            starting_location="RU",
+            ending_location="RS",
+            coordinates=[
+                [-22.817089698597993, -47.07237071352017],
+                [-22.817089698597993, -47.071255630213884],
+                [-22.81827640679855, -47.07052653728283],
+                [-22.818474190493244, -47.06893968796235],
+                [-22.821322243844875, -47.06782460465607],
+                [-22.820887128438063, -47.06709551172503],
+                [-22.822667137220368, -47.066109091877166],
+                [-22.821796913611948, -47.064693793834564],
+                [-22.820491567770727, -47.066109091877166],
+                [-22.818157736443844, -47.06387892526458],
+                [-22.81807862281659, -47.06274563326717],
+                [-22.815902980056652, -47.062574081989275],
+            ],
+            tags=["caminhada", "cultural", "histórica", "lazer"],
+            image="route_images/cover_image_unicamp.jpg",
         )
 
         add_route(
@@ -154,28 +178,4 @@ class Command(BaseCommand):
             ],
             tags=["corrida", "ciclovia"],
             image="route_images/cover_image_unicamp.jpg",
-        )
-
-        add_route(
-            user=User.objects.get(username="Clara Carvalho"),
-            title="Tour dos Bandejões: RU RA RS",
-            description="Uma rota utilitária que interliga os três principais restaurantes universitários (RU, RA e RS). Ideal para calouros ou para quem precisa decidir onde almoçar.",
-            starting_location="RU",
-            ending_location="RS",
-            coordinates=[
-                [-22.817089698597993, -47.07237071352017],
-                [-22.817089698597993, -47.071255630213884],
-                [-22.81827640679855, -47.07052653728283],
-                [-22.818474190493244, -47.06893968796235],
-                [-22.821322243844875, -47.06782460465607],
-                [-22.820887128438063, -47.06709551172503],
-                [-22.822667137220368, -47.066109091877166],
-                [-22.821796913611948, -47.064693793834564],
-                [-22.820491567770727, -47.066109091877166],
-                [-22.818157736443844, -47.06387892526458],
-                [-22.81807862281659, -47.06274563326717],
-                [-22.815902980056652, -47.062574081989275],
-            ],
-            tags=["caminhada", "cultural", "histórica", "lazer"],
-            image="route_images/Bandejao.jpg",
         )
